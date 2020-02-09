63a84f61909077e179283d776a8456878c432469
Sung Kyungmo <skm280@naver.com>
2020-02-09 17:38:37 +0900

    Error Handling

--- main/ErrorHandling/retry.playground/Contents.swift
+++ main/ErrorHandling/retry.playground/Contents.swift
@@ -26,33 +26,35 @@ import RxSwift
 /*:
  # retry
  */
-
+// 옵저버블에서 에러가 발생하면 구독을 해지하고 새로운 구독을 시작
+// 파라미터에 최대 재시도 횟수 + 1(첫시도)
 let bag = DisposeBag()
 
 enum MyError: Error {
-   case error
+  case error
 }
 
 var attempts = 1
 
 let source = Observable<Int>.create { observer in
-   let currentAttempts = attempts
-   print("#\(currentAttempts) START")
-   
-   if attempts < 3 {
-      observer.onError(MyError.error)
-      attempts += 1
-   }
-   
-   observer.onNext(1)
-   observer.onNext(2)
-   observer.onCompleted()
-         
-   return Disposables.create {
-      print("#\(currentAttempts) END")
-   }
+  let currentAttempts = attempts
+  print("#\(currentAttempts) START")
+
+  if attempts < 3 {
+    observer.onError(MyError.error)
+    attempts += 1
+  }
+
+  observer.onNext(1)
+  observer.onNext(2)
+  observer.onCompleted()
+
+  return Disposables.create {
+    print("#\(currentAttempts) END")
+  }
 }
 
 source
-   .subscribe { print($0) }
-   .disposed(by: bag)
+  .retry(5) // 4번 재시도
+  .subscribe { print($0) }
+  .disposed(by: bag)
